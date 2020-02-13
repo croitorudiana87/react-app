a4d0537592cd0ff14effc4ee77efad821895f1ae
Diana Hriscanu <diana.hriscanu@ing.com>
2020-02-13 13:29:08 +0200

    Updates

---
+++ precache-manifest.67ec3f07259f3821f49a5e7fd29731bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2558ae29e4d12cc9d81f60dbb7e8fea",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9cd8e7063f62459b2ccc",
+    "url": "/react-app/static/css/main.39cdaa9f.chunk.css"
+  },
+  {
+    "revision": "84d33f5ada1254b8e3e3",
+    "url": "/react-app/static/js/2.abb5a9d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.abb5a9d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cd8e7063f62459b2ccc",
+    "url": "/react-app/static/js/main.a6e0b474.chunk.js"
+  },
+  {
+    "revision": "bf85471ac02e2eade778",
+    "url": "/react-app/static/js/runtime-main.ab57843b.js"
+  }
+]);
